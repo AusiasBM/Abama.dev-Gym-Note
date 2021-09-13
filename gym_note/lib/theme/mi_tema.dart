b6926b2e8234c442f828216c0429a98f67febf43
Ausias <ausiasbm@gmail.com>
2021-09-13 13:45:21 +0200

    Estructura base del proyecto

---
+++ gym_note/lib/theme/mi_tema.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final miTema = ThemeData.light().copyWith(
+  backgroundColor: Colors.white,
+  appBarTheme: AppBarTheme(
+    color: Colors.white,
+    textTheme: TextTheme(
+      headline6: TextStyle(color: Colors.black),
+    ),
+  ),
+);
